cdf8a23c3466e62f7712fd2a010e079b3f2061d0
Luana-Fraporti <luanasfraporti@gmail.com>
2021-09-14 18:24:12 -0300

    projeto

---
+++ lista-logica/index.js
@@ -0,0 +1,113 @@
+// EXERCICIO 1
+
+// const altura = Number(prompt("Valor da altura"))
+// onst largura = Number(prompt("valor da largura"))
+
+// console.log("Valor total do triangulo é?", altura*largura)
+
+// EXERCICIO 2
+
+// const anoAtual = Number(prompt("Que ano estamos?"))
+// const anoNascimento = Number(prompt("Que ano nasceu?"))
+
+// console.log("Quantos anos tem o usuario?", anoAtual-anoNascimento)
+
+// EXERCICIO 3
+
+// const peso = Number(prompt("Qual o seu peso?"))
+// const altura = Number(prompt("Qual a sua altura?"))
+
+// console.log("Valor do IMC é?", peso / (altura*altura))
+
+// EXERCICIO 4
+
+// const nome = prompt("Qual a seu nome?")
+// const idade = prompt("Qual a sua idade?")
+// const email = prompt("Qual a seu email?")
+
+// console.log("Qual o seu nome?", nome,"Qual a sua idade?", idade, "Qual o seu email", email)
+
+// EXERCICIO 5
+
+// const usuario = ["Qual a sua cor favorita?" ]
+// const corFavorita = ["Roxo", "Amarelo", "Marrom"]
+
+// console.log(usuario)
+// console.log (corFavorita)
+
+// EXERCICIO 6 
+
+// const nome = prompt("QUAL SEU NOME COMPLETO?")
+// console.log ("QUAL SEU NOME COMPLETO", nome)
+
+// EXERCICIO 7
+
+// const custoEspetaculo=Number(prompt("Qual o valor do espetaculo?"))
+// const valorIngresso=Number(prompt("Qual o valor unitário do Ingresso?"))
+
+// console.log("Quantos ingressos tem que vender?", custoEspetaculo/valorIngresso)
+
+// EXERCICIO 8
+
+ // const log = true
+// const tamanhoLargura = prompt("A largura tem 1m?")
+// const tamanhoAltura = prompt("A altura tem 1m?")
+
+// console.log(tamanhoLargura===tamanhoAltura)
+
+// EXERCICIO 9
+
+// const nomeIgual = prompt("A escrita do nome é igual do B, luana")
+// const nomeDiferente = prompt ("A escrita do nome é igual do A, LUANA")
+
+// console.log(nomeDiferente===nomeIgual)
+
+// EXERCICIO 10
+
+// const anoAtual = Number(prompt("Qual ano estamos?"))
+// const anoNascimento = Number(prompt("Qual ano do seu nascimento?"))
+// const anoIdentidade = Number(prompt("Qual ano emitiu o RG?"))
+
+// console.log(anoAtual<=anoNascimento)
+//console.log(anoAtual>=anoNascimento)
+// console.log (anoNascimento>=anoAtual)
+
+// EXERCICIO 11 - refer
+
+// const bissextos400 = Number(prompt("todos anos multimos de 400?"))
+// const bissextos4 = Number(prompt("todos anos os multiplos de 4"))
+// const nãoBissextos = Number(prompt("todos os demais anos"))
+
+// console.log(bissextos400>=bissextos4)
+// console.log (bissextos400<=bissextos4)
+// console.log(bissextos400===nãoBissextos)
+
+// EXERCICIO 12 rever
+
+// const Idade = prompt("Você tem mais de 18 anos?")
+// const ensinoMedio = prompt("você tem o ensimo medio completo?")
+// const horario = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
+
+// console.log(2021>=1991)
+// console.log(8<=24)
+// console.log(ensinoMedio===ensinoMedio)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
