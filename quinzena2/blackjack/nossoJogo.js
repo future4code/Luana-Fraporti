8dd79988ef2a5ffec18825433e486b7b200b2a28
Luana-Fraporti <luanasfraporti@gmail.com>
2021-09-28 22:18:26 -0300

    loops

---
+++ quinzena2/blackjack/nossoJogo.js
@@ -0,0 +1,41 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+// 1) exercicio
+
+// console.log ("Boas vindas ao jogo de Blackjack!)
+
+// 2) exercicio
+
+// const confirmacaoJogo = prompt("O jogo acabou", "Nova Rodada")
+
+// if(confirm ("Quer Iniciar uma rodada?")) {
+	
+// } if 
+// 	(confirmacaoJogo === ok){
+//    console.log ("Nova Rodada")
+//  } else {
+//  (confirmacaoJogo === cancel)
+//     console.log("O jogo acabou")
+//  }
+
+
+
+
+let arrayOriginal = [10,20,30,40,50],
+
+for (let item of arrayOriginal) {
+
+ console.log(item);
+
+}
+
