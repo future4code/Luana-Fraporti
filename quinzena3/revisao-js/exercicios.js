74980185040eb352e81e4b12ee2fbe232eb53c52
Luana-Fraporti <luanasfraporti@gmail.com>
2021-10-07 21:21:30 -0300

    revisao

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -11,11 +11,12 @@ return arrayDeNumeros
 function retornaNumerosParesElevadosADois(array) {
   let numerosParesElevados2 = []
   for (let item of array){
-    if (item %2 == 0){
+    if (item %2 === 0){
       let elevarPor2 = item * item
       numerosParesElevados2.push(elevarPor2)
     }
   }
+  return numerosParesElevados2
 }
 
 
@@ -31,11 +32,14 @@ function retornaNumerosPares(array) {
 
 // EXERCÍCIO 04
 function retornaMaiorNumero(array) {
-let arrayMaior = []
-for (let i=0; i<array.length;i++){
-  arrayMaior.push(array.length-i)
+let numeroMaior = array[0]
+  for (let i=0; i<array.length;i++){
+ let numero = array [i];
+ if (numero > numeroMaior) {
+   numeroMaior = numero;
 }
-return arrayMaior
+}
+return numeroMaior
 }
 
 // EXERCÍCIO 05
@@ -46,16 +50,43 @@ function retornaQuantidadeElementos(array) {
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
+  const booleano1 = true
+  const booleano2 = false
+  const booleano3 = !booleano2 
+  const booleano4 = !booleano3 
+
+  let respostasBooleanos = [
+    booleano1 && booleano2 && !booleano4,
+    (booleano1 && booleano2) || !booleano3, 
+    (booleano2 || booleano3) && (booleano4 || booleano1),
+    !(booleano2 && booleano3) || !(booleano1 && booleano3),
+    !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+  ]
+return respostasBooleanos
 
 }
 
 // EXERCÍCIO 07
 function retornaNNumerosPares(n) {
-
+let parametro = []
+for (let i=0;i < n;i++){
+  parametro.push(i*2)
+}
+return parametro
 }
 
 // EXERCÍCIO 08
 function checaTriangulo(a, b, c) {
+ const tresLadosIguais = "Equilátero"
+ const doisLadosIguais = "Isósceles"
+ const todosLadosDiferentes = "Escaleno"
+ if (a===b && a===c){
+   return tresLadosIguais
+ } else if (a !== b && a !== c){
+   return todosLadosDiferentes
+ } else { 
+   return doisLadosIguais
+ }
   // return 'Escaleno'
   // return 'Equilátero'
   // return 'Isósceles'
@@ -63,6 +94,27 @@ function checaTriangulo(a, b, c) {
 
 // EXERCÍCIO 09
 function comparaDoisNumeros(num1, num2) {
+const maior 
+const menor
+const diferenca
+
+if (num1 > num2){
+  maior = num1
+  menor = num2
+} else {
+  menor = num1
+  maior = num2
+}
+let divisivel = maior % menor === 0
+
+let valordif = maior - menor
+
+let objeto = 
+{
+  maiorNumero: maior,
+  maiorDivisivelPorMenor1: divisivel,
+
+}
   // Formato do objeto a ser retornado:
   // {
   //   maiorNumero: X,
@@ -73,7 +125,22 @@ function comparaDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 10
 function segundoMaiorEMenor(array) {
+[2,4,5,9,3]
+[2,4,5,9,3]
 
+[0,1,2,3,4]
+
+[2,3,4,5,9]
+
+const novaArray = []
+
+let contador = 0
+for (itemComparado of array){
+if (item > itemComparado){
+  contador = contador +1 
+}
+}
+novaArray [contador]=item
 }
 
 // EXERCÍCIO 11
