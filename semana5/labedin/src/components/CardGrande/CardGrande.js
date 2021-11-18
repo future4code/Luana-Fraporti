7de1d5efbf7b6b563fb6a76374b6c9695c4ac5d1
Luana-Fraporti <luanasfraporti@gmail.com>
2021-11-17 22:18:35 -0300

    insta4

--- semana5/labedin/src/components/CardGrande/CardGrande.js
+++ semana5/labedin/src/components/CardGrande/CardGrande.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import './CardGrande.css'
+import styled from 'styled-components';
+
+const Elemento = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 100px;
+    height: 100px;
+    width: 100px;
+    margin-right: 10px;
+    border-radius: 50%;
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start; `
+
+    const Foto = styled.img`
+    width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;`
+    
+    const Letra = styled.h4`
+    margin-bottom: 10px;
+    display: center; `
 
 
 function CardGrande(props) {
     return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
+        <Elemento>
+            <Foto src={ props.imagem } />
             <div>
-                <h4>{ props.nome }</h4>
+                <Letra>{ props.nome }</Letra>
                 <p>{ props.descricao }</p>
             </div>
-        </div>
+    </Elemento>
     )
 }
+
 
 export default CardGrande;
